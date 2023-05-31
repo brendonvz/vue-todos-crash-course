938c5c66963bebe5fdab32655713bbe4766239f3
brendonvz <32501372+brendonvz@users.noreply.github.com>
2023-05-31 13:48:07 -0400

    update read me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# vue-todos
+# Vue To-dos
 
-This tutorial was built by following the tutorial from John Komarnicki developing with Vue 3 in Vite.
+This app was built by following the tutorial from John Komarnicki developing with Vue 3 in Vite.
+
+In this tutorial I learned the basics of Vue JS and then built a app that allows you to create update and delete to do items and also save this information to local storage
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about container">
     <h1>This is the about page</h1>
-		<p>Vue Todos is a CRUD Todo tracking application created by Brendon van Zanten by following the Vue 3 Crash Course by John Komarnicki.</p>
+c		<p>Vue Todos is a CRUD Todo tracking application created by Brendon van Zanten by following the Vue 3 Crash Course by John Komarnicki.</p>
 		<p>Track your daily task's using this todo tracker with abilities to save, complete, edit & delete todos.</p>
   </div>
 </template>
