fc64afcaad67bba1793946a5c9b0cc37da8225ac
brendonvz <32501372+brendonvz@users.noreply.github.com>
2023-05-31 11:08:35 -0400

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about container">
+    <h1>This is the about page</h1>
+		<p>Vue Todos is a CRUD Todo tracking application created by Brendon van Zanten by following the Vue 3 Crash Course by John Komarnicki.</p>
+		<p>Track your daily task's using this todo tracker with abilities to save, complete, edit & delete todos.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+		padding: 25px 16px;
+  }
+	p, h1 {
+		margin-bottom: 12px;
+	}
+}
+</style>
